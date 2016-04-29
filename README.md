0a9cc67fb7ce603841bae0ee285faa79cdcfd8a5
Vichit Mike Sitthideth <vmsitth@uw.edu>
2016-04-29 04:33:22 -0700

    COMPLETE

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# data-exploration
-Building a Data Exploration Tool
+For your visual layout, describe why it is an optimal choice for expressing your data (~ 1 paragraph). Then, for each controlling element, detail how it enables your user to ask different questions (~1 paragraph). You should include specific examples of insights you discovered through the use of these tools. 
+
+
+Visual Layout
+-----------------------
+The reason why I used a stacked bar chart is because it shows the highest number of violent crime in each Precinct and Beat but still shows the other numbers as well.  The opacity of the bars are set to be lighter so that that the more filled the bar is, the more frequent that number of crime occurs in that Beat.  The use of opacity helps show weight of frequency.  And position along a common scale (axes) helps in comparing each Beat to one another.  This is to help with the effectiveness of the chart.
+
+Other choices made were with the background color, which fit well with showing the opacity difference between the bars, using red for the bars, and the text color.  The red bars and their opacities were an expressive design choice to relate back to violence (red relating to blood) and show weight of the data.  I discovered that doing so actually showed a possible average range of violent crime incidents for the Beats during the entire 2014 year.
+
+
+Controlling Elements
+-----------------------
+The controlling elements are the buttons that indicate a selection of Precincts.  What this allows to do is ask questions like which precinct had the most violent crime incidents in 2014 or which beat in a certain precinct had the most violent crimes.  These questions can be answered by clicking through the different Precincts and viewing the chart.
+
+Through the choice of having the rect elements stack up on each other with lower opacity, I found another question could be asked: out of the total number of violent crimes during 2014 for a beat, what is the average range of these incidents?
--- js/main.js
+++ js/main.js
@@ -1,84 +1,100 @@
+// Javascript to load the data of Major Crimes in Seattle during
+// 	2014 and puts them into a stacked bar chart
+
 $(function() {
 	d3.csv('data/Statistics_Major_Crimes__2014__by_Precinct_and_Beat.csv', function(error, data) {
 		var xScale, yScale, currentData;
 
-		console.log(data);
-
+		// Variable holds the default Precinct that will show
 		var precinct = 'NORTH';
-		var crime = 'MAJOR_CRIMES_TOTAL';
 
+		// Margin dimensions 
 		var margin = {
 			left: 70,
 			bottom: 100,
 			top: 50,
-			right: 50,
+			right: 50
 		};
 
-		var height = 600 - margin.bottom - margin.top;
+		// Variables that will hold the height and width
+		var height = 550 - margin.bottom - margin.top;
 		var width = 1000 - margin.left - margin.right;
 
-		var svg = d3.select('#vis')
+		//
+		var svg = d3.select('#div')
 					.append('svg')
-					.attr('height', 600)
+					.attr('height', 550)
 					.attr('width', 1000);
 
+		//	Appending g to the svg
 		var g = svg.append('g')
 					.attr('transform', 'translate(' + margin.left + "," + margin.top + ')')
 					.attr('height', height)
 					.attr('width', width);
 
+
+		//	Creates the labels and text areas for the Axes
 		var xAxisLabel = svg.append('g')
 							.attr('transform', 'translate(' + margin.left + ',' + (height + margin.top) + ')')
 							.attr('class', 'title')
 
 		var yAxisLabel = svg.append('g')
-							.attr('transform', 'translate(' + margin.left + ',' + (margin.top) + ')')
+							.attr('transform', 'translate(' + margin.left + ',' + margin.top + ')')
 							.attr('class', 'title')
 
 		var xAxisText = svg.append('text')
-							.attr('transform', 'translate(' + (margin.left + width / 2) + ',' + (height + margin.top *2) + ')')
+							.attr('transform', 'translate(' + (margin.left + width / 2) + ',' + (height + margin.top + 50) + ')')
 							.attr('class', 'label')
 
 		var yAxisText = svg.append('text')
-							.attr('transform', 'translate(' + (margin.left - 60) + ',' + (margin.top + height) + ') rotate(-90)')
+							.attr('transform', 'translate(' + (margin.left - 50) + ',' + (margin.top + height - 80) + ') rotate(-90)')
 							.attr('class', 'label')
 
-
+		//  Sets the scales for the x and y axes
 		var setScales = function(data) {
+
+			//	If the BEAT is empty, create All, which will represent All Beats
 			var beat = data.map(function(d) { if (d.BEAT == "") {return 'All'} return d.BEAT});
 
-			xScale = d3.scale.ordinal().rangeBands([0, width], .2).domain(beat);
+			xScale = d3.scale.ordinal().rangeBands([0, width], .1).domain(beat);
 
-			var yMin = d3.min(data, function(d) {return +d.HOMICIDE} );
+			var yMin = d3.min(data, function(d) {return +d.VIOLENT_CRIMES_TOTAL} );
 
-			var yMax = d3.max(data, function(d) {return +d.HOMICIDE} );
+			var yMax = d3.max(data, function(d) {return +d.VIOLENT_CRIMES_TOTAL} );
 
 			yScale = d3.scale.linear().range([height, 0]).domain([0, yMax]);
 		}
 
+		//	Sets the x labels for x-axis
 		var setAxes = function() {
 
 			var xAxis = d3.svg.axis()
 						.scale(xScale)
 						.orient('bottom')
 
+
+			//	Sets the y labels for y-axis and forces 'n' number format (Precision)
 			var yAxis = d3.svg.axis()
 						.scale(yScale)
 						.orient('left')
+						.tickFormat(d3.format('n'));
 
-			xAxisLabel.transition().duration(1000).call(xAxis);
+			//	Labels transition in at 500 ms
+			xAxisLabel.transition().duration(500).call(xAxis);
+			yAxisLabel.transition().duration(500).call(yAxis);
 
-			yAxisLabel.transition().duration(1000).call(yAxis);
-
-			xAxisText.text('Beat')
-			yAxisText.text('Number of Crime Occurrences (' + precinct + ')')
+			//	Text label for axes
+			xAxisText.text('Police Beat (Territory)');
+			yAxisText.text('Number of Crime Occurrences');
 		}
 
+		//	Filters the data by the Precinct that is passed in
 		var filterData = function() {
 			currentData = data.filter(function(d) {
 				return d.PRECINCT == precinct
 			})
 
+			//	Alphabetical sort 
 			.sort(function(a,b) {
 				if(a.BEAT < b.BEAT) return -1;
 				if(a.BEAT > b.BEAT) return 1;
@@ -86,13 +102,15 @@ $(function() {
 			});
 		}
 
+		//	Draw function that 
 		var draw = function(data) {
 			setScales(data)
-
 			setAxes()
 
+			//	Selects all the rects to work with
 			var bars = g.selectAll('rect').data(data);
 
+			//	Get entering elements and positions them
 			bars.enter().append('rect')
 				.attr('x', function(d) {return xScale(d.BEAT)} )
 				.attr('y', height)
@@ -101,34 +119,33 @@ $(function() {
 				.attr('class', 'bar')
 				.attr('title', function(d) {return d.BEAT});
 
+			//	Elements no longer in data are removed
 			bars.exit().remove();
 
+			//	Makes bars and transitions
 			bars.transition()
-				.duration(1000)
+				.duration(500)
 				.delay(function(d,i) {return i*50})
 				.attr('x', function(d) {return xScale(d.BEAT)})
-				.attr('y', function(d) {return yScale(d.HOMICIDE)})
-				.attr('height', function (d) {return height - yScale(d.HOMICIDE)})
+				.attr('y', function(d) {return yScale(d.VIOLENT_CRIMES_TOTAL)})
+				.attr('height', function (d) {return height - yScale(d.VIOLENT_CRIMES_TOTAL)})
 				.attr('width', xScale.rangeBand())
 				.attr('title', function(d) {return d.BEAT});
 		}
 
-		// Change event to switches input element to what is chosen
+		// Change event to switch input element to the Precinct button chosen
 		$("input").on('change', function() {
 			var val = $(this).val();
 			var isPrecinct = $(this).hasClass('precinct');
 
-			console.log(isPrecinct);
 			if(isPrecinct) {
 				precinct = val;
-			} else {
-				crime = val;
 			}
-			console.log(precinct);
 			filterData();
 			draw(currentData);
 		});
 
+		//	Calls Functions to filters data and creates the bars
 		filterData()
 		draw(currentData)
 	});
